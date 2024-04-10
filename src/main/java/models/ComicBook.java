7ba0367aac4d907644d2d4519e25e19a18bb5492
Anthony <145401343+AnthonyP365@users.noreply.github.com>
2024-04-10 07:52:15 -0400

    added instance variables

--- src/main/java/models/ComicBook.java
+++ src/main/java/models/ComicBook.java
@@ -2,4 +2,7 @@ package models;
 
 public class ComicBook {
 //    The comic book object
+    private String name;
+    private int id;
+    private float price;
 }
--- src/main/java/models/VideoGame.java
+++ src/main/java/models/VideoGame.java
@@ -2,4 +2,8 @@ package models;
 
 public class VideoGame {
 //    The video game object
+    private String name;
+    private int id;
+    private String genre;
+    private float price;
 }
